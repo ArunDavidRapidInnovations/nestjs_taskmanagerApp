38704ae39cb3a4d0bbd5eb9e125d4a035772384d
Arun David <davidreddy293@gmail.com>
2021-08-11 11:14:45 +0530

    Wrote a custom decorator to better understand how decorators work

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,35 @@
 import { Injectable } from '@nestjs/common';
 
+function addMr() {
+  return function (target: Object, key: string | symbol) {
+    let val = target[key];
+
+    const getter = () => {
+      return `${val} 👽`;
+    };
+
+    const setter = (next) => {
+      console.log('Adding Mr To the name');
+      val = `Mr. ${next}`;
+    };
+
+    Object.defineProperty(target, key, {
+      get: getter,
+      set: setter,
+      enumerable: true,
+      configurable: true,
+    });
+  };
+}
 @Injectable()
 export class AppService {
+  @addMr()
+  name = 'Arun';
+
   getHello(): string {
-    return 'Hello World!';
+    console.log(this.name);
+    const messageToPrint = `Welcome ${this.name}`;
+
+    return messageToPrint;
   }
 }
