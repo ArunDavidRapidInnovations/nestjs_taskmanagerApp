a5c858933014e1eb084e328e65be9a9c73a80f13
Arun David <davidreddy293@gmail.com>
2021-08-11 15:57:08 +0530

    Code for the Section 2 of the nestjs couse

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,17 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { get } from 'http';
 import { CreateTaskDto } from './dto/create-task.dto';
-import { Task } from './task.model';
+import { GetTaskFilleterDto as GetTaskFilleterDto } from './dto/get-tasks-filter.dto';
+import { Task, TaskStatus } from './task.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
@@ -9,8 +19,12 @@ export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getTasks() {
-    return this.tasksService.getAllTasks();
+  getTasks(@Query() filterDto: GetTaskFilleterDto): Task[] {
+    if (Object.keys(filterDto).length) {
+      return this.tasksService.getTasksWithFilters(filterDto);
+    } else {
+      return this.tasksService.getAllTasks();
+    }
   }
 
   @Post()
@@ -21,5 +35,15 @@ export class TasksController {
   @Get(':id')
   getTaskById(@Param('id') id): Task {
     return this.tasksService.getTaskById(id);
+  }
+
+  @Patch(':id/status')
+  updateTaskStatusById(@Param('id') id, @Body('status') status) {
+    return this.tasksService.updateTaskStatusById(id, status);
+  }
+
+  @Delete(':id')
+  deleteTaskById(@Param('id') id): void {
+    return this.tasksService.deleteTaskById(id);
   }
 }
