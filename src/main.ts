aeef1a610646669a296bee12241e0f67bcff9ae7
Arun David <davidreddy293@gmail.com>
2021-08-12 17:19:16 +0530

    Made the backend port from status to env variable

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,11 @@ import { TransformInterceptor } from './transform.interceptor';
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
-  await app.listen(3000);
-  logger.log('Application is running on port 3000');
+  const appPort = process.env.PORT || 3000;
+  await app.listen(appPort);
+  logger.log(`Application is running on port ${appPort}`);
 }
 bootstrap();
